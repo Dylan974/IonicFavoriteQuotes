2cef02bbe3e4555d98dbd00601f05c3a98d3a9e7
Dylboy974 <mugidylan@gmail.com>
2017-09-03 18:11:57 +0400

    add quotes data into quotesPage ans output them with card component

--- src/pages/library/library.ts
+++ src/pages/library/library.ts
@@ -4,13 +4,15 @@ import { NavController, NavParams } from 'ionic-angular';
 import { Quote } from '../../data/quote.interface';
 import quotes from '../../data/quotes';
 
+import { QuotesPage } from '../quotes/quotes';
+
 @Component({
   selector: 'page-library',
   templateUrl: 'library.html',
 })
 export class LibraryPage implements OnInit {
   quoteCollection: {category: string, quotes: Quote[], icon: string}[];
-
+  quotesPage = QuotesPage;
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
